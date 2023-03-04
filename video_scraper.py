bed1d610921d256ad7bf914e6d3174b4f5da6882
Saschax99 <dol.sascha@googlemail.com>
2023-03-04 02:16:34 +0100

    v0.2 editing successfully

--- video_scraper.py
+++ video_scraper.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
-from pathlib import Path
 from requests import get
 import requests
 from bs4 import BeautifulSoup
@@ -20,11 +19,12 @@ class VideoScraper:
         self.keyword = "beautiful car"
         self.baseurl_args = "?orientation=portrait"
         self.songs_path = "songs"
+        self.temp_path = "temp"
         self.results_path = "results"
         self.url = self.baseurl + self.keyword + self.baseurl_args
-
+        self.scroll_amount = 100
+        
         self.video_links = []
-        self.object_path = None
         if execute_browser:
             self.options = webdriver.ChromeOptions()
             self.options.add_argument("--lang=en")
@@ -44,16 +44,19 @@ class VideoScraper:
 
         self.browser.get(url)
         WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((By.XPATH, "//video/source")))
+        for i in range(self.scroll_amount):  # scroll to bottom to load more objects
+            self.browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+            time.sleep(1)
+    
         soup = BeautifulSoup(self.browser.page_source, 'lxml')
         links = soup.find_all("source")
         for link in links:
             self.video_links.append(link["src"])
 
-        self.object_path = Path(self.results_path, self.keyword)
-        if not os.path.isdir(self.object_path):
-            os.mkdir(self.object_path)
+        if not os.path.isdir(self.temp_path):
+            os.mkdir(self.temp_path)
 
-        with open(f'{Path(self.object_path, self.keyword)}.txt', 'w', newline='', encoding="utf-8") as f:
+        with open(f'{os.path.join(self.temp_path, self.keyword)}.txt', 'w', newline='', encoding="utf-8") as f:
             for elem in self.video_links:
                 f.write(f"{elem}\n")
         return self.video_links
@@ -76,48 +79,52 @@ class VideoScraper:
                 count += 1
         return count
 
+    def __select_object_from_folder(self, path, count) -> str:
+        return os.listdir(path)[count]
+    
     def download_video_series(self, video_links: list = None):
-        if self.object_path is None:
-            self.object_path = Path(self.results_path, self.keyword)
-
         if video_links is None:
             video_links = []
-            with open(f'{Path(self.object_path, self.keyword)}.txt', 'r', encoding="utf-8") as f:
+            with open(f'{os.path.join(self.temp_path, self.keyword)}.txt', 'r', encoding="utf-8") as f:
                 video_links.append(f.read().split("\n"))
 
-        self.amount_of_songs = self.__get_folder_count(".mp4")
-        # if amount_of_songs <= 0:
-        #    raise RuntimeError(f"no songs in '{self.songs_path}' found!")
+        self.amount_of_songs = self.__get_folder_count(".mp3")
+        if self.amount_of_songs <= 0:
+            raise RuntimeError(f"no songs in '{self.songs_path}' found!")
         print(self.amount_of_songs)
 
         for link in video_links[0]:
-            file_name = link.split("/")[-1].split("?")[0]
+            self.file_name = link.split("/")[-1].split("?")[0]
             print(link)
-            print(file_name)
-            print(f"Start to downloading video: {file_name}")
+            print(self.file_name)
+            print(f"Start to downloading video: {self.file_name}")
             # create response object
             r = requests.get(link, stream=True)
             # download started
-            with open(Path(self.object_path, file_name), 'wb') as f:
+            self.file_path = os.path.join(self.temp_path, self.file_name)
+            with open(self.file_path, 'wb') as f:
                 for chunk in r.iter_content(chunk_size=1024 * 1024):
                     if chunk:
                         f.write(chunk)
 
-            print(f"{file_name} downloaded!")
+            print(f"{self.file_name} downloaded!")
+            self.edit_video()
             exit()
-            self.edit_video(file_name)
 
-    def edit_video(self, file):
-        # editing the video
-        list = random.choice(range(1, self.amount_of_songs))
-        clip = mymovie.VideoFileClip(file)
+    def edit_video(self):
+        ran_int = random.choice(range(0, self.amount_of_songs))
+        music_clip = self.__select_object_from_folder(self.songs_path, ran_int)
+        print(self.file_path)
+        print(music_clip)
+        clip = mymovie.VideoFileClip(self.file_path)
         clip_duration = clip.duration
-        audioclip = mymovie.AudioFileClip(f"songs/audio{list}.mp3").set_duration(clip_duration)
+        audioclip = mymovie.AudioFileClip(os.path.join(self.songs_path, music_clip)).set_duration(clip_duration)
         new_audioclip = mymovie.CompositeAudioClip([audioclip])
         finalclip = clip.set_audio(new_audioclip)
-        # location = os.path.join("C:\\Users\\python\\Desktop\\videos", f"video{i}.mp4")
-        finalclip.write_videofile(f"videos/vid{i}.mp4", fps=60)
-        print("%s has been edited!\n" % file)
+        final_path = os.path.join(self.results_path, self.file_name)
+        finalclip.write_videofile(f"{final_path}.mp4", fps=60)
+        print(f"{self.file_path} has been edited!")
+        exit()
 
 if __name__ == "__main__":
     vs = VideoScraper(False)
