11dea1a940d0acc1b8d0505a49b3c1aa93c430ab
Saschax99 <dol.sascha@googlemail.com>
2023-03-05 18:14:01 +0100

    v0.3

--- video_scraper.py
+++ video_scraper.py
@@ -4,59 +4,80 @@ import undetected_chromedriver as webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import os
 from requests import get
 import requests
 from bs4 import BeautifulSoup
-from itertools import islice
 import moviepy.editor as mymovie
 import random
+import uuid
 
+songs_path = "songs"
+temp_path = "temp"
+results_path = "results"
 
-class VideoScraper:
-    def __init__(self, execute_browser=True):
+class OsTools:
+    @staticmethod
+    def get_folder_count(path, extensions: str = None) -> int:
+        list_dir = os.listdir(path)
+        count = 0
+        for file in list_dir:
+            if extensions is not None:
+                if file.endswith(extensions):  # eg: '.txt'
+                    count += 1
+            else:
+                count += 1
+        return count
+
+    @staticmethod
+    def select_specific_object_from_folder(path, count) -> str:
+        return os.listdir(path)[count]
+    
+
+class YoutubeShortsFetcher:
+    def __init__(self):
         self.baseurl = "https://www.pexels.com/search/videos/"
-        self.keyword = "beautiful car"
+        self.keyword = "natur"
         self.baseurl_args = "?orientation=portrait"
-        self.songs_path = "songs"
-        self.temp_path = "temp"
-        self.results_path = "results"
         self.url = self.baseurl + self.keyword + self.baseurl_args
-        self.scroll_amount = 100
         
         self.video_links = []
-        if execute_browser:
-            self.options = webdriver.ChromeOptions()
-            self.options.add_argument("--lang=en")
 
-            self.browser = webdriver.Chrome(use_subprocess=True, options=self.options)
-            self.browser.maximize_window()
+    def __establish_connection(self, url: str = None) -> None:
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument("--lang=en")
 
-    def get_video_links_from_keyword(self, url: str = None) -> list:
-        """get video links
-
-        :param url:
-        :return:
-        """
-        timeout = 30
+        self.browser = webdriver.Chrome(use_subprocess=True, options=self.options)
+        self.browser.maximize_window()
         if url is None:
             url = self.url
-
         self.browser.get(url)
+    
+    def get_video_links_from_keyword(self, url: str = None) -> list:
+        timeout = 30
         WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((By.XPATH, "//video/source")))
-        for i in range(self.scroll_amount):  # scroll to bottom to load more objects
-            self.browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-            time.sleep(1)
+        
+        reached_page_end = False
+        last_height = self.browser.execute_script("return document.body.scrollHeight")
+        while not reached_page_end:
+            self.browser.find_element_by_xpath('//body').send_keys(Keys.END)   
+            time.sleep(1.5)
+            new_height = self.browser.execute_script("return document.body.scrollHeight")
+            if last_height == new_height:
+                    reached_page_end = True
+            else:
+                last_height = new_height
     
         soup = BeautifulSoup(self.browser.page_source, 'lxml')
-        links = soup.find_all("source")
+        links = soup.find_all("source") 
         for link in links:
-            self.video_links.append(link["src"])
+            self.video_links.append(link.get("src"))
 
-        if not os.path.isdir(self.temp_path):
-            os.mkdir(self.temp_path)
+        if not os.path.isdir(temp_path):
+            os.mkdir(temp_path)
 
-        with open(f'{os.path.join(self.temp_path, self.keyword)}.txt', 'w', newline='', encoding="utf-8") as f:
+        with open(f'{os.path.join(temp_path, self.keyword)}.txt', 'w', newline='', encoding="utf-8") as f:
             for elem in self.video_links:
                 f.write(f"{elem}\n")
         return self.video_links
@@ -67,33 +88,23 @@ class VideoScraper:
 
     def close(self) -> None:
         self.browser.close()
-
-    def __get_folder_count(self, extensions: str = None) -> int:
-        list_dir = os.listdir(self.songs_path)
-        count = 0
-        for file in list_dir:
-            if extensions is not None:
-                if file.endswith(extensions):  # eg: '.txt'
-                    count += 1
-            else:
-                count += 1
-        return count
-
-    def __select_object_from_folder(self, path, count) -> str:
-        return os.listdir(path)[count]
     
     def download_video_series(self, video_links: list = None):
         if video_links is None:
             video_links = []
-            with open(f'{os.path.join(self.temp_path, self.keyword)}.txt', 'r', encoding="utf-8") as f:
+            video_links_path = os.path.join(temp_path, self.keyword)
+            with open(f'{video_links_path}.txt', 'r', encoding="utf-8") as f:
                 video_links.append(f.read().split("\n"))
 
-        self.amount_of_songs = self.__get_folder_count(".mp3")
+        self.amount_of_songs = OsTools.get_folder_count(songs_path, ".mp3")
         if self.amount_of_songs <= 0:
-            raise RuntimeError(f"no songs in '{self.songs_path}' found!")
+            raise RuntimeError(f"no songs in '{songs_path}' found!")
         print(self.amount_of_songs)
 
-        for link in video_links[0]:
+        for index, link in enumerate(video_links[0]):
+            if link is None:
+                continue
+            
             self.file_name = link.split("/")[-1].split("?")[0]
             print(link)
             print(self.file_name)
@@ -101,7 +112,7 @@ class VideoScraper:
             # create response object
             r = requests.get(link, stream=True)
             # download started
-            self.file_path = os.path.join(self.temp_path, self.file_name)
+            self.file_path = os.path.join(temp_path, f"{uuid.uuid1().hex}.mp4")
             with open(self.file_path, 'wb') as f:
                 for chunk in r.iter_content(chunk_size=1024 * 1024):
                     if chunk:
@@ -109,29 +120,89 @@ class VideoScraper:
 
             print(f"{self.file_name} downloaded!")
             self.edit_video()
-            exit()
+            
+            # removing temp file and row in .txt file
+            os.remove(self.file_path)
+            
+            with open(f"{video_links_path}.txt", 'r+') as f:
+                lines = f.readlines()
+                f.seek(0)
+                f.writelines(line for i, line in enumerate(lines) if i != 0)
+                f.truncate()
+                
+            print(f"{self.file_path} removed!")
 
     def edit_video(self):
         ran_int = random.choice(range(0, self.amount_of_songs))
-        music_clip = self.__select_object_from_folder(self.songs_path, ran_int)
+        music_clip =OsTools.select_specific_object_from_folder(songs_path, ran_int)
         print(self.file_path)
         print(music_clip)
-        clip = mymovie.VideoFileClip(self.file_path)
-        clip_duration = clip.duration
-        audioclip = mymovie.AudioFileClip(os.path.join(self.songs_path, music_clip)).set_duration(clip_duration)
-        new_audioclip = mymovie.CompositeAudioClip([audioclip])
-        finalclip = clip.set_audio(new_audioclip)
-        final_path = os.path.join(self.results_path, self.file_name)
-        finalclip.write_videofile(f"{final_path}.mp4", fps=60)
-        print(f"{self.file_path} has been edited!")
-        exit()
+        
+        
+        video = mymovie.VideoFileClip(self.file_path)
+        video_duration = video.duration
+        audio = mymovie.AudioFileClip(os.path.join(songs_path, music_clip)).set_duration(video_duration).audio_fadeout(.33)
+        #new_audioclip = mymovie.CompositeAudioClip([audioclip])
+        video_with_music = video.set_audio(audio)
+        final_path = os.path.join(results_path, f"{uuid.uuid1().hex}.mp4")
+        video_with_music.write_videofile(f"{final_path}", fps=60, codec="libx264")
+        video.close()
+        
+        print(f"{final_path} has been edited!")
+        
+class YoutubeShortsUploader:
+    def __init__(self) -> None:
+        self.url = "https://studio.youtube.com"
 
+        self.__establish_connection()
+
+    def __establish_connection(self, url: str = None) -> None:
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument("--lang=en")
+        self.options.add_argument("--log-level=3")
+        self.options.add_argument("user-data-dir=C:\\Users\dolsa\\AppData\\Local\\Google\\Chrome\\User Data")
+        self.options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+
+        self.browser = webdriver.Chrome(use_subprocess=True, options=self.options)
+        self.browser.maximize_window()
+        if url is None:
+            url = self.url
+        self.browser.get(url)
+        
+    def upload_video_series(self):
+        if OsTools.get_folder_count(results_path) <= 0:
+            raise RuntimeError(f"no videos in '{results_path}' found!")
+            
+        time.sleep(3)
+        upload_button = self.browser.find_element(By.XPATH, '//*[@id="upload-icon"]')
+        upload_button.click()
+        time.sleep(5)
+
+        file_input = self.browser.find_element(By.XPATH, '//*[@id="content"]/input')
+        simp_path = 'videos/vid{}.mp4'.format(str(i+1))
+        abs_path = os.path.abspath(simp_path)
+        
+        file_input.send_keys(abs_path)
+
+        time.sleep(7)
+
+        next_button = self.browser.find_element(By.XPATH, '//*[@id="next-button"]')
+        for i in range(3):
+            next_button.click()
+            time.sleep(5)
+
+        done_button = self.browser.find_element(By.XPATH, '//*[@id="done-button"]')
+        done_button.click()
+        time.sleep(5)    
+    
+    
 if __name__ == "__main__":
-    vs = VideoScraper(False)
-    #video_links = vs.get_video_links_from_keyword()
-    # print(video_links)
+    vsf = YoutubeShortsFetcher()
+    #video_links = vsf.get_video_links_from_keyword()
+    #print(video_links)
+    vsf.download_video_series()
+    #vsf.__establish_connection()
     #vs.close()
-    vs.download_video_series()
 
     # print(video_links)
 
